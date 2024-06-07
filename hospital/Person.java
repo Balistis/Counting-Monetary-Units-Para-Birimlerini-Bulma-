e2c61486e0d6fe6db5d5d65e2e3400c0fe63f163
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2024-06-07 19:18:56 +0300

    Add files via upload

---
+++ hospital/Person.java
@@ -0,0 +1,27 @@
+//package hospitalOOP;
+
+public class Person {
+    private String name;
+    private double salary;
+
+    public Person(String name, double Salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
